0a6c20afb7e31913168c6fdfefad9945b99002f2
Théodore <theodore@ch-le.fr>
2026-04-14 18:47:38 +0200

    cerr -> clog

--- src/node.hpp
+++ src/node.hpp
@@ -93,7 +93,7 @@ namespace commons {
 
         virtual std::string debugValue() const { return ""; }
 
-        virtual void debugDisplay(std::ostream &flow = std::cerr, int indent = 0) const {
+        virtual void debugDisplay(std::ostream &flow = std::clog, int indent = 0) const {
             for (int i = 0; i < indent; i++) {
                 flow << "\t";
             }
